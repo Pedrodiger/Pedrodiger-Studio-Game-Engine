05a15f5b83de4ec19cb7aa80a902bebe4e9f1090
Pedrodiger <81391552+Pedrodiger@users.noreply.github.com>
2022-05-09 17:22:13 -0400

    Create MeshRenderer.java

---
+++ src/com/base/engine/components/MeshRenderer.java
@@ -0,0 +1,26 @@
+package com.base.engine.components;
+
+import com.base.engine.rendering.Material;
+import com.base.engine.rendering.Mesh;
+import com.base.engine.rendering.RenderingEngine;
+import com.base.engine.rendering.Shader;
+
+public class MeshRenderer extends GameComponent
+{
+	private Mesh     m_mesh;
+	private Material m_material;
+
+	public MeshRenderer(Mesh mesh, Material material)
+	{
+		this.m_mesh = mesh;
+		this.m_material = material;
+	}
+
+	@Override
+	public void Render(Shader shader, RenderingEngine renderingEngine)
+	{
+		shader.Bind();
+		shader.UpdateUniforms(GetTransform(), m_material, renderingEngine);
+		m_mesh.Draw();
+	}
+}
