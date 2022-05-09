be407c76116b0b35fdffffa80fead092d17f7af1
Pedrodiger <81391552+Pedrodiger@users.noreply.github.com>
2022-05-09 17:22:55 -0400

    Create PointLight.java

---
+++ src/com/base/engine/components/PointLight.java
@@ -0,0 +1,42 @@
+package com.base.engine.components;
+
+import com.base.engine.core.Vector3f;
+import com.base.engine.rendering.Attenuation;
+import com.base.engine.rendering.Shader;
+
+public class PointLight extends BaseLight
+{
+	private static final int COLOR_DEPTH = 256;
+
+	private Attenuation m_attenuation;
+	private float       m_range;
+	
+	public PointLight(Vector3f color, float intensity, Attenuation attenuation)
+	{
+		super(color, intensity);
+		this.m_attenuation = attenuation;
+
+		float a = attenuation.GetExponent();
+		float b = attenuation.GetLinear();
+		float c = attenuation.GetConstant() - COLOR_DEPTH * GetIntensity() * GetColor().Max();
+
+		this.m_range = (float)((-b + Math.sqrt(b * b - 4 * a * c))/(2 * a));
+
+		SetShader(new Shader("forward-point"));
+	}
+
+	public float GetRange()
+	{
+		return m_range;
+	}
+
+	public void SetRange(float range)
+	{
+		this.m_range = range;
+	}
+
+	public Attenuation GetAttenuation()
+	{
+		return m_attenuation;
+	}
+}
